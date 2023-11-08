ad9ab2de05c77195afa8394ac9165b41610bc144
HWDEV <114353227+Dohung123hba@users.noreply.github.com>
2023-11-08 14:23:29 +0700

    Update Login.js

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,8 +1,8 @@
 function Login(){
   return(
     <>
-      <input type="text" placeholder="username" required />
-      <input type="password" placeholder="password" required />
+      <input type="text" placeholder="username" required style={{marginRight: "20px"}}/>
+      <input type="password" placeholder="password" required style={{marginRight: "20px"}}/>
       <button>Đăng nhập</button>
     </>
   )
